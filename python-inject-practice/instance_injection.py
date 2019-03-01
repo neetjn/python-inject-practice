d99192af6ec1a199f671a2a0118cff0d8dfe689d
John Nolette <jnolette@transparent.com>
2019-03-01 15:49:19 -0500

    Adding comments

--- python-inject-practice/instance_injection.py
+++ python-inject-practice/instance_injection.py
@@ -13,20 +13,24 @@ class Database:
 
 
 def save_user_age(name, age):
+    """Saves some user's age in our theoretical database"""
     db = inject.instance(Database)
     db.save(name, age)
 
 
 def get_user_age(name):
+    """Fetch a user's age in our theoretical database"""
     db = inject.instance(Database)
     return db.get(name)
 
 
 # Configuration for database binding
 def db_config(binder):
+    # define a single instance of the database to use here
     binder.bind(Database, Database())
 
 
+# configure our injection with the database config
 inject.configure(db_config)
 
 save_user_age('john', 10)
