a183d8c04d80418955232b87a9fa8593ee22fc08
John Nolette <jnolette@transparent.com>
2019-03-02 13:53:33 -0500

    Finishing base param experiment

--- python-inject-practice/param_injection.py
+++ python-inject-practice/param_injection.py
@@ -1,3 +1,35 @@
 import inject
 
 
+class Database:
+    """Some theoretical database provider."""
+    data = {}
+
+    def get(self, key: str):
+        return self.data.get(key)
+
+    def save(self, key: str, value: all):
+        self.data.setdefault(key, value)
+
+
+@inject.params(client=Database)
+def set_user(username: str, value: all, client: Database=None):
+    return client.save(username, value)
+
+
+@inject.params(client=Database)
+def get_user(username: str, client: Database=None):
+    return client.get(username)
+
+
+# Configuration for database binding
+def db_config(binder):
+    # define a single instance of the database to use here
+    binder.bind(Database, Database())
+
+
+# configure our injection with the database config
+inject.configure(db_config)
+
+set_user('john', 1)
+assert get_user('john') == 1
