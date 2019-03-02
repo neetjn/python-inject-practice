3831d7157ed10cac525cd177b884c4f03a4ebf22
John Nolette <jnolette@transparent.com>
2019-03-02 15:05:19 -0500

    Adding comments

--- python_inject_practice/experiments/param_injection.py
+++ python_inject_practice/experiments/param_injection.py
@@ -4,21 +4,25 @@ from python_inject_practice.core.database import Database
 
 @inject.params(client=Database)
 def set_user(username: str, value: all, client: Database=None):
+    """Save user data in a theoretical database"""
     return client.save(username, value)
 
 
 @inject.params(client=Database)
 def get_user(username: str, client: Database=None):
+    """Get user data from a theoretical database"""
     return client.get(username)
 
 
 @inject.autoparams()
 def set_user_auto(username: str, value: all, client: Database=None):
+    """Save user data in a theoretical database"""
     return client.save(username, value)
 
 
 @inject.autoparams()
 def get_user_auto(username: str, client: Database=None):
+    """Get user data from a theoretical database"""
     return client.get(username)
 
 
