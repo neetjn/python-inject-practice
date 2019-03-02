a4687762df3b9475c869ca59cb868516aa6c6499
John Nolette <jnolette@transparent.com>
2019-03-02 14:01:23 -0500

    Adding autoparams

--- python-inject-practice/param_injection.py
+++ python-inject-practice/param_injection.py
@@ -22,6 +22,16 @@ def get_user(username: str, client: Database=None):
     return client.get(username)
 
 
+@inject.autoparams()
+def set_user_auto(username: str, value: all, client: Database=None):
+    return client.save(username, value)
+
+
+@inject.autoparams()
+def get_user_auto(username: str, client: Database=None):
+    return client.get(username)
+
+
 # Configuration for database binding
 def db_config(binder):
     # define a single instance of the database to use here
@@ -31,5 +41,9 @@ def db_config(binder):
 # configure our injection with the database config
 inject.configure(db_config)
 
+# experiment with params inject
 set_user('john', 1)
 assert get_user('john') == 1
+# experiment with autoparams
+set_user_auto('doe', 1)
+assert get_user_auto('doe') == 1
